8512be5c65d1a19703d387a3e064fb95461b8ef2
vitwits <vitwits@gmail.com>
2021-08-19 23:02:14 +0300

    loader added

--- script.js
+++ script.js
@@ -4,11 +4,25 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
+// Show loading 
+function loading(){
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// Hide loading 
+function coplete(){
+  loader.hidden = true;
+  quoteContainer.hidden = false;
+}
+
 // Show New Quote
 function newQuote(){
+  loading();
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
   if (quote.author){
     authorText.textContent = quote.author;
@@ -20,10 +34,15 @@ function newQuote(){
   } else {
     quoteText.classList.remove('long-quote');
   }
+
+  //Set quote and hide loader
   quoteText.textContent = quote.text;
+  coplete();
 }
 
+// Fetch quotes from API
 async function getQuotes() {
+  loading();
   const apiUrl = 'https://type.fit/api/quotes';
   try {
     const response = await fetch(apiUrl); // response will not be populated until data is fetched
@@ -35,5 +54,14 @@ async function getQuotes() {
   }
 }
 
+// Tweet Quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, '_blank');
+}
+
+// Twitter button listener
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 
 getQuotes();
